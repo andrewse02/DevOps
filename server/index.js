09cd216eb56387372449ac3bad01afcb7beeb17c
Andrew Elliott <andrewelliott159@gmail.com>
2022-02-24 11:42:14 -0700

    Fixed port

--- server/index.js
+++ server/index.js
@@ -10,6 +10,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/../public/index.html"));
 });
 
-app.listen(4545, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
